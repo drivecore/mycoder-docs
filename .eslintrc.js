d772f2b9713f6619460ca7b7d967dbd2ea31504e
Ben Houston <neuralsoft@gmail.com>
2025-03-07 01:44:07 -0500

    Add Docker container and GitHub workflow for Cloud Run deployment
    - Created Dockerfile for building the documentation site
    - Added GitHub workflow for CI/CD to deploy to Cloud Run
    - Added linting workflow and ESLint configuration
    - Added .dockerignore file to exclude unnecessary files
    - Added documentation on the deployment process
    - Added lint script and ESLint dependencies to package.json
    
    Resolves #2

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'build/',
+    '.docusaurus/',
+    'dist/',
+    '**/*.d.ts',
+  ],
+};
